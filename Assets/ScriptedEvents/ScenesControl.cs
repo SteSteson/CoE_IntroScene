22e7763e0b7a0a55a198b5bea4d37d5ced77e2df
NeacsuAdrian <adyatbass@yahoo.com>
2021-09-09 19:11:07 +0300

    scene3
    ok

---
+++ Assets/ScriptedEvents/ScenesControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenesControl : MonoBehaviour
+{
+    public GameObject SceneToDisable;
+    public GameObject SceneToEnable;
+
+    public void SwapScenes()
+    {
+        SceneToDisable.SetActive(false);
+        SceneToEnable.SetActive(true);
+    }
+}
